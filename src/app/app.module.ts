30cf79bd4eb4454978e2901c4fd350221a34438c
gonzaga8200 <gonzaga8200@gmail.com>
2019-03-28 02:44:45 +0100

    Add header and students

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StudentsComponent } from './students/students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule,
